a9349e24094a37438f5b5ba46756e63961b55f11
Gudujian <59535419+ForGuDuJian@users.noreply.github.com>
2024-04-08 09:58:28 +0800

    train.py

--- yolov5/train.py
+++ yolov5/train.py
@@ -421,7 +421,7 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
 
 def parse_opt(known=False):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', type=str, default='yolov5x.pt', help='initial weights path')
+    parser.add_argument('--weights', type=str, default='', help='initial weights path')
     parser.add_argument('--cfg', type=str, default='models/yolov5x.yaml', help='model.yaml path')
     parser.add_argument('--data', type=str, default='data/coco.yaml', help='dataset.yaml path')
     parser.add_argument('--hyp', type=str, default='data/hyps/hyp.scratch-high.yaml', help='hyperparameters path')
